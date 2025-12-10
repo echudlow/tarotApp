c1cd4c0c6f225dc18f2956cfeb8d0ea3472a5fca
Elijah Hudlow <elijah.hudlow@gmail.com>
2025-12-10 14:44:36 -0500

    initial commit

---
+++ Tarot/Tarot/TarotApp.swift
@@ -0,0 +1,32 @@
+//
+//  TarotApp.swift
+//  Tarot
+//
+//  Created by Elijah Hudlow on 12/10/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TarotApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Tarot/TarotTests/TarotTests.swift
@@ -0,0 +1,17 @@
+//
+//  TarotTests.swift
+//  TarotTests
+//
+//  Created by Elijah Hudlow on 12/10/25.
+//
+
+import Testing
+@testable import Tarot
+
+struct TarotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
